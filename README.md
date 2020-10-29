bc37811bcdfce66c064909f1b08112a494103128
José Luis Simón <joseluis@jsimon.kit.local>
2020-10-29 10:49:51 -0300

    Version inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+Informática Aplicada ECA 
+Ejemplo de polimorfismo en C++
+Versión 1.0 2020
---
+++ src/IA2020_LEDS.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : IA2020_LEDS.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "Microcontrolador.h"
+
+using namespace std;
+
+int main() {
+	Microcontrolador micro( "FRDM-KL46" );
+	micro.ejecutar();
+
+	return 0;
+}
